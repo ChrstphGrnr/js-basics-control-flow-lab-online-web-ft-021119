ee9cafceefc231206b811a1a24055a26fc3f330a
Christoph Gruener <christophgruener@me.com>
2019-05-21 17:00:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let response;
+  if distance >2500
+      response = "No can do."
+    elseif distance <= 400
+      response = "This one is on me!"
+    else
 }
 
 function ternaryCheckCity(){
