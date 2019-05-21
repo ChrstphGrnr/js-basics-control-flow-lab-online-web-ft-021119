6881c67be409b931e48ec9fb53ec0a8ce8191867
Christoph Gruener <christophgruener@me.com>
2019-05-21 17:13:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let response;
+  if ( distance > 2500) {
+      response = "No can do.";
+    }
+    else if ( distance <= 400 ) {
+      response = "This one is on me!";
+    }
+    else if ( distance > 2000 ) {
+      response = "I will gladly take your thirty bucks.";
+    }
+    return response
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let response;
+  if (city === "NYC") {
+    response = "Ok, sounds good."
+  } else {
+    response = "No go."
+  }
+  return response
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+  if (tip === 'generous') {
+    response = 'Thank you so much.';
+  }
+  else if (tip === 'not as generous') {
+    response = 'Thank you.';
+  }
+  else {
+    response = 'Bye.';
+  }
+  return response
 }
